198b32cca4cbf31bf4a9b10c70f7f43e799eb836
Yuancheng Zhang <old.far.hot@gmail.com>
2017-11-21 10:57:40 -0600

    add demo project files

---
+++ centaurs-test-plugin-java/src/main/java/Plugin.java
@@ -0,0 +1,67 @@
+import okhttp3.*;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Plugin {
+
+    OkHttpClient client = new OkHttpClient();
+    MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+
+    // App information
+    String server = "https://robot-service.centaurstech.com/api/chat";
+    String appkey = "open-demo";
+    String appsecret = "123456789";
+    String nickname = "张三李四王老五";
+
+    // Get hardware fingerprint
+    String uid = GetNetworkAddress.GetAddress("mac");
+
+    public String getResponse(String ask) throws Exception {
+        // Prepare verify string
+        String now = (new Date()).getTime() + "";
+        String verify = Md5.digest(appsecret + uid + now);
+
+        // Generate request body
+        String bodyStr = String.format(
+                "appkey=%s&timestamp=%s&uid=%s&verify=%s&msg=%s&nickname=%s",
+                appkey, now, uid, verify, ask, nickname);
+
+        RequestBody body = RequestBody.create(mediaType,
+                bodyStr);
+        Request request = new Request.Builder()
+                .url(server)
+                .post(body)
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .addHeader("cache-control", "no-cache")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        String res = response.body().string();
+        System.out.println(res);
+
+        JSONObject json = new JSONObject(res);
+        if (json.has("data"))
+            System.out.println("Appended data: " + json.getJSONObject("data"));
+        else
+            System.out.println("There is no extra data");
+        return json.getString("msg");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Plugin plugin = new Plugin();
+        String message = plugin.getResponse("HELLO");
+        System.out.println(message);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            message = sc.nextLine();
+            message = plugin.getResponse(message);
+            System.out.println(message);
+        }
+
+    }
+
+}
